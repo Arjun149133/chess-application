be98213ba027e3043dc2fdd567b5ea47a8409887
Arjun Yadav <124586416+Arjun149133@users.noreply.github.com>
2025-04-19 21:06:02 +0530

    Update config.ts 2

--- apps/web/lib/config.ts
+++ apps/web/lib/config.ts
@@ -1,5 +1,5 @@
 // export const BACKEND_URL = "http://localhost:8081";
 // export const WS_URL = "http://localhost:8080";
 
-export const BACKEND_URL = "https://chess-be-six.vercel.app/";
+export const BACKEND_URL = "https://chess-be-six.vercel.app";
 export const WS_URL = "https://chess-websocket-api.arjunyadav.website";
